0a899c76ee50a9dcd0ea5254a5296eefcb4244f3
oubouali.hakim.per <oubouali.hakim.per@gmail.com>
2024-02-10 14:00:33 +0000

    add terraform blockN

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_providers {
-    
-  }
+  
 }
 
 provider "aws" {
   
+}
+
+resource "aws_s3_bucket" "R1" {
+  for_each = toset(var.delta)
+  bucket = each.value
+}
+
+data "aws_s3_bucket" "d1" {
+  for_each = aws_s3_bucket.R1
+  bucket = each.value.bucket
 }
